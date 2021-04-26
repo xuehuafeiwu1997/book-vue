5543ea2e28e84a1c13af6ddd5a055513e5fdc2cb
xumingyang <xumingyang@greatyun.com>
2021-04-26 10:04:14 +0800

    添加index之后才可以点击

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
         <el-menu>
           <el-submenu v-for="(item,index) in $router.options.routes" :index="index+''">
             <template slot="title"><i class="el-icon-setting"></i>{{item.name}}</template>
-            <el-menu-item v-for="item2 in item.children">
+            <el-menu-item v-for="(item2,index2) in item.children" :index="index + '-' +index2">
               {{item2.name}}
             </el-menu-item>
           </el-submenu>

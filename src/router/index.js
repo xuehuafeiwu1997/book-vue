4d85291c5084f6d2114367aeae5012b082bc00e3
xumingyang <xumingyang@greatyun.com>
2021-04-26 13:59:38 +0800

    修改导航栏名字

--- src/router/index.js
+++ src/router/index.js
@@ -1,61 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import PageOne from "../views/PageOne.vue";
-import PageTwo from "../views/PageTwo.vue";
-import PageThree from "../views/PageThree.vue";
-import PageFour from "../views/PageFour.vue";
-// import App from "../App.vue"
+import BookManage from "../views/BookManage.vue";
+import AddBook from "../views/AddBook.vue";
 import Index from "../views/Index.vue"
 
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // },
   {
     path: '/',
-    name: '导航一',
+    name: '图书管理',
     component: Index,
-    redirect: "/pageOne",
+    redirect: "/bookManage",
     children:[
       {
-        path: '/pageOne',
-        name: '页面一',
-        component: PageOne
+        path: '/bookManage',
+        name: '查询图书',
+        component: BookManage
       },
       {
-        path: '/pageTwo',
-        name: '页面二',
-        component: PageTwo
-      }
-    ]
-  },
-  {
-    path:'/navigation',
-    name:'导航二',
-    component: Index,
-    children: [
-      {
-        path: '/pageThree',
-        name: '页面三',
-        component: PageThree
-      },
-      {
-        path: '/pageFour',
-        name: '页面四',
-        component: PageFour
+        path: '/addBook',
+        name: '添加图书',
+        component: AddBook
       }
     ]
   }
